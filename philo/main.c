abb3a15cb42b623dadcc8fc42669a0a69908e18d
Vladimir Angirov <angirov@posteo.net>
2022-07-26 23:59:02 +0200

    include death variable, add i++ in joining, no exit(), print dead message in main thread

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: vangirov <vangirov@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:19:29 by vangirov          #+#    #+#             */
-/*   Updated: 2022/07/26 20:40:33 by vangirov         ###   ########.fr       */
+/*   Updated: 2022/07/26 23:48:08 by vangirov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void	ft_join_threads(t_wisdom *wisdom)
 	i = 0;
 	while (i < wisdom->num_ph)
 	// {
-		pthread_join(wisdom->threads[i], NULL);
+		pthread_join(wisdom->threads[i++], NULL);
 		// i++;
 	// }
 	// printf("Joined all\n");
@@ -101,15 +101,14 @@ void	ft_god(t_wisdom *wisdom)
 	{
 		if (ft_usec_diff(wisdom->philos[i]->last_meal) > us_to_die)
 		{
-			// wisdom->death = 1;
+			wisdom->death = 1;
 			break ;
 		}
 		i = (i + 1) % wisdom->num_ph;
 		// printf("Check philo %2d\n", i);
 	}
-	ft_print_action(wisdom->philos[i], "died", ft_usec_diff(wisdom->philos[i]->last_meal) / 1000);
+	ft_print(wisdom->philos[i], "died", ft_usec_diff(wisdom->philos[i]->last_meal) / 1000);
 	// free...
-	exit(42);
 }
 
 int	main(int argc, char **argv)
@@ -144,3 +143,78 @@ int	main(int argc, char **argv)
 // timestamp_in_ms X is sleeping
 // timestamp_in_ms X is thinking
 // timestamp_in_ms X died
+
+// void	ft_philo_liffe(t_philo *philo)
+// {
+// 	int	fork_right;
+// 	int	fork_left;
+// 	long	us_to_eat;
+// 	long	us_to_sleep;
+
+// 	us_to_eat = philo->wisdom->us_to_eat;
+// 	us_to_sleep = philo->wisdom->us_to_sleep;
+// 	// if (philo->index == philo->wisdom->num_ph - 1)
+// 	// {
+// 		fork_left = (philo->index + 1) % philo->wisdom->num_ph;
+// 		fork_right = philo->index;
+// 	// }
+// 	// else
+// 	// {
+// 	// 	fork_left = philo->index;
+// 	// 	fork_right = (philo->index + 1) % philo->wisdom->num_ph;
+// 	// }
+// 	if (philo->index %2 == 0)
+// 		usleep(1000);
+// 	usleep(1000 * philo->index);
+// 	while (!philo->wisdom->death)
+// 	{	
+// 		while(!philo->wisdom->death)
+// 		{
+// 			if (!philo->wisdom->forks[fork_right])
+// 			{
+// 				pthread_mutex_lock(philo->wisdom->mtx_forks + fork_right);
+// 				// ft_print_action(philo, "has taken a right fork", fork_right);
+// 				philo->wisdom->forks[fork_right] = 1;
+// 				if (!philo->wisdom->forks[fork_left])
+// 				{
+// 					pthread_mutex_lock(philo->wisdom->mtx_forks + fork_left);
+// 					gettimeofday(&philo->last_meal, NULL);
+// 					ft_print_action(philo, "has taken a right fork", fork_right);//////////////////////////
+// 					ft_print_action(philo, "has taken a left  fork", fork_left);
+// 					philo->wisdom->forks[fork_left] = 1;
+// 					break ;
+// 				}
+// 				philo->wisdom->forks[fork_right] = 0;
+// 				pthread_mutex_unlock(philo->wisdom->mtx_forks + fork_right);
+// 			}
+// 			else if(!philo->wisdom->forks[fork_left])
+// 			{
+// 				pthread_mutex_lock(philo->wisdom->mtx_forks + fork_left);
+// 				// ft_print_action(philo, "has taken a left  fork", fork_left);
+// 				philo->wisdom->forks[fork_left] = 1;
+// 				if (!philo->wisdom->forks[fork_right])
+// 				{
+// 					pthread_mutex_lock(philo->wisdom->mtx_forks + fork_right);
+// 					gettimeofday(&philo->last_meal, NULL);
+// 					ft_print_action(philo, "has taken a left  fork", fork_left); ////////////////////////////
+// 					ft_print_action(philo, "has taken a right fork", fork_right);
+// 					philo->wisdom->forks[fork_right] = 1;
+// 					break ;
+// 				}
+// 				philo->wisdom->forks[fork_left] = 0;
+// 				pthread_mutex_unlock(philo->wisdom->mtx_forks + fork_left);
+// 			}
+// 		}
+		
+// 		ft_print_action(philo, "is eating", -1);
+// 		usleep(us_to_eat);
+		
+// 		philo->wisdom->forks[fork_left] = 0;
+// 		pthread_mutex_unlock(philo->wisdom->mtx_forks + fork_left);
+// 		philo->wisdom->forks[fork_right] = 0;
+// 		pthread_mutex_unlock(philo->wisdom->mtx_forks + fork_right);
+// 		ft_print_action(philo, "is sleeping", -1);
+// 		usleep(us_to_sleep);
+// 		ft_print_action(philo, "is thinking", -1);
+// 	}
+// }
