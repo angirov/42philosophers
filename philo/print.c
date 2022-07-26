88e2c380cd5750d378858346d92da338bba96624
Vladimir Angirov <angirov@posteo.net>
2022-07-26 23:55:47 +0200

    printing is a detached thread

--- philo/print.c
+++ philo/print.c
@@ -6,7 +6,7 @@
 /*   By: vangirov <vangirov@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 20:41:10 by vangirov          #+#    #+#             */
-/*   Updated: 2022/07/26 20:41:14 by vangirov         ###   ########.fr       */
+/*   Updated: 2022/07/26 23:20:39 by vangirov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,58 @@ long	ft_print_time(void)
 	return (now.tv_sec * 1000 + now.tv_usec / 1000);
 }
 
-void	ft_print_action(t_philo *philo, const char *action, int fork)
+// void	ft_print_action(t_philo *philo, const char *action, int fork)
+// {
+// 	pthread_mutex_lock(&philo->wisdom->mtx_print);
+// 	printf("[%ld] ", ft_print_time());
+// 	if (fork >=0)
+// 		printf("Philo %2d %s [%d]\n", philo->index, action, fork);
+// 	else
+// 		printf("Philo %2d %s\n", philo->index, action);
+// 	pthread_mutex_unlock(&philo->wisdom->mtx_print);
+// }
+
+typedef	struct s_print
+{
+	t_philo		*philo;
+	const char	*action;
+	int			num;
+} t_print;
+
+void	ft_print_action_thread(t_print *task)
+{
+	t_philo		*philo;
+	const char	*action;
+	int			num;
+	
+	philo = task->philo;
+	action = task->action;
+	num = task->num;
+	pthread_mutex_lock(&philo->wisdom->mtx_print);
+	printf("[%ld] ", ft_print_time());
+	if (num >=0)
+		printf("Philo %2d %s [%d]\n", philo->index, action, num);
+	else
+		printf("Philo %2d %s\n", philo->index, action);
+	pthread_mutex_unlock(&philo->wisdom->mtx_print);
+	free(task);
+	pthread_exit(NULL);
+}
+
+void	ft_print_action(t_philo *philo, const char *action, int num)
+{
+	pthread_t	thread;
+	t_print		*task;
+
+	task = malloc(sizeof(t_print));
+	task->philo = philo;
+	task->action = action;
+	task->num = num;
+	pthread_create(&thread, NULL, (void *)(&ft_print_action_thread), task);
+	pthread_detach(thread);
+}
+
+void	ft_print(t_philo *philo, const char *action, int fork)
 {
 	pthread_mutex_lock(&philo->wisdom->mtx_print);
 	printf("[%ld] ", ft_print_time());
